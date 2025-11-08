8f4cec351ed32b3b8cf107de2f2bfeee6d3acb44
DmytroLysachenko <dlysachenko98@gmail.com>
2025-11-08 15:54:04 +0100

    feat/ initialized project

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using SafeVault.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProblemDetails();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSingleton<IUserSubmissionService, UserSubmissionService>();
+
+builder.Services.AddScoped<ISecureUserRepository>(_ =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    if (string.IsNullOrWhiteSpace(connectionString))
+    {
+        throw new InvalidOperationException("Connection string 'DefaultConnection' is missing.");
+    }
+
+    return new SecureUserRepository(connectionString);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler();
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
